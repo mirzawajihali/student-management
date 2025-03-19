fbfd6ad5907974139fec861ce78e4151c6fe879f
mirzawajihali <mirzawajihali00@gmail.com>
2025-03-19 18:02:13 +0600

    Add server code and update client

--- hall-management-client/src/pages/Jobs/MyPostedJobs.jsx
+++ hall-management-client/src/pages/Jobs/MyPostedJobs.jsx
@@ -1,9 +1,81 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useAuth from '../../hooks/useAuth';
+import JobCard from './JobCard';
+import { Link } from 'react-router-dom';
+import { MdDelete } from 'react-icons/md';
+import { FaEdit } from 'react-icons/fa';
+import { TbListDetails } from 'react-icons/tb';
 
 const MyPostedJobs = () => {
+
+    const {user} = useAuth();
+    const [jobs, setJobs] = useState([]);
+
+    useEffect(()=>{
+        fetch(`http://localhost:5000/jobs?email=${user.email}`)
+        .then(res=>res.json())
+        .then(data=>setJobs(data))
+    },[user.email])
+
+
     return (
-        <div>
-            <h1>My Posted Jobs</h1>
+        <div className='mb-30 lg:max-w-7xl mx-auto'>
+            <h1 className='text-3xl text-center font-bold my-10'>My Posted Jobs</h1>
+            <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>
+          
+        </th>
+        <th>Name</th>
+        <th>Salary</th>
+        <th>Status</th>
+        <th>Deadline</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+    
+    {jobs.map(job=>{
+                return(<tr>
+                    <th>
+                     
+                    </th>
+                    <td>
+                      <div className="flex items-center gap-3">
+                        <div className="avatar">
+                          <div className="mask mask-squircle h-12 w-12">
+                            <img
+                              src={job.company_logo}
+                              alt="Avatar Tailwind CSS Component" />
+                          </div>
+                        </div>
+                        <div>
+                          <div className="font-bold">{job.title}</div>
+                          <div className="text-sm opacity-50">{job.company}</div>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                    {job.salaryRange.min.toLocaleString()} - {job.salaryRange.max.toLocaleString()} {job.salaryRange.currency.toUpperCase()}
+                    </td>
+                    <td>No Application Yet</td>
+                    <td>{job.applicationDeadline}</td>
+                    <th className='flex gap-2'>
+                      <Link to={`/jobs/${job._id}`}><button className="btn text-blue-500 "><TbListDetails /></button></Link>
+                      <button className="btn text-green-600 "><FaEdit /></button>
+                      <button className="btn text-red-700 "><MdDelete /></button>
+                    </th>
+                  </tr>
+                )
+            })}
+    </tbody>
+  
+  </table>
+</div>
+          
         </div>
     );
 };
---
+++ student-management-server/index.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+require('dotenv').config();
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.sk4ge.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+
+
+
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+    // jobs related apis 
+    const jobsCollection = client.db('studentManagement').collection('jobs');
+    const jobApplicationsCollection = client.db('studentManagement').collection('jobApplications');
+
+    app.get("/jobs", async(req, res)=>{
+      const email = req.query.email;
+      let query = {};
+      if(email){
+        query = {hr_email: email};
+      }
+      const cursor = jobsCollection.find(query);
+      const jobs = await cursor.toArray();
+      res.send(jobs);
+    })
+
+    app.post("/jobs", async(req, res)=>{
+        const job = req.body;
+        const result = await jobsCollection.insertOne(job);
+        res.send(result);
+    })
+
+    app.get("/jobs/:id", async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const job = await jobsCollection.findOne(query);
+        res.send(job);
+    })
+
+    app.get('/job-applications', async(req, res)=>{
+        const email = req.query.email;
+        const query = {email: email};
+        const result =await jobApplicationsCollection.find(query).toArray() ;
+
+        for(const application of result){
+            const jobQuery = {_id: new ObjectId(application.jobId)};
+            const job = await jobsCollection.findOne(jobQuery);
+           if(job){
+           application.title = job.title;
+           application.company = job.company;
+           application.location = job.location;
+           application.description = job.description;
+           application.salaryRange = job.salaryRange;
+           
+           }
+        }
+      
+        res.send(result);
+    })
+
+    app.post('/job-applications', async(req, res)=>{
+        const jobApplication = req.body;
+        const result = await jobApplicationsCollection.insertOne(jobApplication);
+        res.send(result);
+    })
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Student Management System');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
